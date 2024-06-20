74245b91bba1d1aa28914a0f79428a6de15a5f92
JasirV <jasirmon133@gmail.com>
2024-06-21 00:20:22 +0530

    Filter

--- src/pages/Admin/OrderManagement.jsx
+++ src/pages/Admin/OrderManagement.jsx
@@ -13,6 +13,10 @@ import Paper from '@mui/material/Paper';
 import AdminSidebar from '../../components/Admin/AdminSidebar';
 import api from '../../axiosInterceptors';
 import OrderDetailsModal from '../../components/Admin/OrderDetailsModal';
+import Loader from '../../components/Loader';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid';
+import { FaFilter } from 'react-icons/fa';
+import FilterModalOrder from '../../components/Admin/modal/FilterModalOrder';
 
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -50,25 +54,42 @@ export default function OrderManagement() {
   const [preOrders, setPreOrders] = useState([]);
   const [selectedOrder, setSelectedOrder] = useState(null);
   const [modalOpen, setModalOpen] = useState(false);
+  const [params,setParams]=useState('')
+  const [loading,setLoading]=useState(false)
+  const [page,setPage]=useState(1)
+  const [toatalpage,setTotalaPage]=useState()
+  const [totalLength,setTotalLength]=useState(0)
+  const [isOpen,setIsOpen]=useState(false)
+  const [filter,setFilter]=useState('')
 
   useEffect(() => {
     fetchAllOrders();
-  }, []);
+  }, [params,page]);
 
   const fetchAllOrders = async () => {
-    try {
-      const response = await api.get('/orders');
+    setLoading(true)
+    try { 
+      const response = await api.get(`/orders`, {params:{params:params,page,filter}});
+      setTotalaPage(response?.data?.totalpage)
+      setTotalLength(response?.data?.totalLength)
       if (response.status === 200) {
+        setLoading(false)
         const orders = response.data.orders;
-        setAllOrders(orders);
-        const bulkOrders = orders.filter(order => order.bulkOrder);
-        setBulkOrders(bulkOrders);
-        const preOrders = orders.filter(order => order.preOrder);
-        setPreOrders(preOrders);
+        if(params==='preorder'){
+          setPreOrders(orders);
+        }else if(params==='bulkorder'){
+          console.log("hau");
+          setBulkOrders(orders);
+        }else{
+
+          setAllOrders(orders);
+        }     
       }
       console.log(response, 'allOrder');
     } catch (error) {
       console.log(error, 'error all orders');
+      setLoading(false)
+
     }
   };
 
@@ -84,12 +105,52 @@ export default function OrderManagement() {
   const handleClose = () => {
     setModalOpen(false);
   };
+  const handleClick=(p)=>{
+    if(p==="prev"&&page>1){
+      setPage(page-1)
+    }else if(p==="next"&&page<toatalpage){
+      setPage(page+1);
+    }else if(typeof p==='number'){
+      setPage(p)
+    }
+  }
+
+  const renderPageNumbers=()=>{
+    let startPage=Math.max(page-2,1);
+    let endPage=Math.min(page+3 ,toatalpage);
+    if(endPage-startPage<3){
+      startPage=Math.max(endPage-3,1)
+    }
+    const pageNumbers=[];
+    for(let i=startPage;i<=endPage;i++){
+      pageNumbers.push(
+        <p
+          key={i}
+          onClick={() => handleClick(i)}
+          className={`relative z-10 inline-flex items-center px-4 py-2 text-sm font-semibold ${
+            page === i
+              ? 'bg-indigo-600 text-white'
+              : 'text-gray-700 hover:bg-gray-50'
+          } focus:z-20 focus-visible:outline h-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer`}
+        >
+          {i}
+        </p>
+      )
+    }
+    return pageNumbers
+  }
 
   return (
+    <>
+    {loading&& <Loader/>}
+   
     <div className='flex w-full h-screen overflow-hidden bg-[#F8F8F8]'>
       <div className='mt-2 min-h-screen'>
         <AdminSidebar />
       </div>
+       {isOpen&&
+    <FilterModalOrder  isOpen={isOpen} setIsOpen={setIsOpen} setFilter={setFilter}/>
+    }
       <Box sx={{ width: '100%', overflow: 'scroll' }}>
         <div className='flex justify-center'>
           <h1 className='pt-4 pl-2'>Orders</h1>
@@ -97,12 +158,18 @@ export default function OrderManagement() {
         <hr />
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-            <Tab label="All" {...a11yProps(0)} />
-            <Tab label="Bulk Orders" {...a11yProps(1)} />
-            <Tab label="Pre Orders" {...a11yProps(2)} />
+            <Tab label="All" {...a11yProps(0)} onClick={()=>setParams('')}/>
+            <Tab label="Bulk Orders" {...a11yProps(1)} onClick={()=>{setParams('bulkorder')}}/>
+            <Tab label="Pre Orders" {...a11yProps(2)} onClick={()=>{setParams('preorder')}}/>
+            <div className=' flex items-center p-2 px-5'>
+        {!isOpen && <FaFilter size={16} onClick={()=>setIsOpen(true)} />}
+      </div>
           </Tabs>
         </Box>
         <CustomTabPanel value={value} index={0}>
+        {isOpen&&
+    <FilterModalOrder  isOpen={isOpen} setIsOpen={setIsOpen} setFilter={setFilter}/>
+    }
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
               <TableHead className='bg-[#0E2C72] '>
@@ -186,6 +253,28 @@ export default function OrderManagement() {
               </Table>
             </TableContainer>
           </CustomTabPanel>
+          <div className='flex justify-end mx-4'>
+          <nav className="isolate inline-flex  -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+            <a
+              onClick={() => handleClick('prev')}
+              className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              <span className="sr-only">Previous</span>
+              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+            </a>
+            <div>
+            {renderPageNumbers()}
+            </div>
+
+            <a
+              onClick={() => handleClick('next')}
+              className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              <span className="sr-only">Next</span>
+              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+            </a>
+          </nav>
+        </div>
         </Box>
         {selectedOrder && (
         <OrderDetailsModal
@@ -195,7 +284,7 @@ export default function OrderManagement() {
         />
       )}
       </div>
-     
+      </>
     
   );
 }
